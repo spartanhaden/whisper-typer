eb08f8c64e996394953b2e8b0658d84591d61265
haden <git@haden.io>
2023-03-16 04:30:50 -0700

    Added tons of customization optionn like selectable inference device and activation key and mic. Also improves error checking and documentation. Also rewrote readme.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A small script that types what you say using whisper while holding a hotkey
 
+I have only tested this on Linux, but it should work on Windows and Mac as well
+
 ## Usage
 
 Install requirements:
@@ -16,4 +18,34 @@ Run the script:
 python3 main.py
 ```
 
-The script currently uses the `base.en` model and the `F16` key for push to talk. I also have only tested this on Linux, but it should work on Windows and Mac as well. Also the script will use the default audio input device.
+## Command line options
+
+```bash
+python3 main.py --help
+```
+
+```
+usage: main.py [-h] [--model_name {tiny.en,tiny,base.en,base,small.en,small,medium.en,medium,large-v1,large-v2,large}] [--mic_device MIC_DEVICE] [--inference_device INFERENCE_DEVICE] [--activation_key ACTIVATION_KEY]
+
+a small script that types what you say using whisper while holding a hotkey
+
+options:
+  -h, --help            show this help message and exit
+  --model_name {tiny.en,tiny,base.en,base,small.en,small,medium.en,medium,large-v1,large-v2,large}
+                        the name of the whisper model to use
+  --mic_device MIC_DEVICE
+                        the name of the pyaudio input device to use
+  --inference_device INFERENCE_DEVICE
+                        the device to run the inference on. can be cpu, cuda, or cuda:<device number> will automatically select the best device if not specified
+  --activation_key ACTIVATION_KEY
+                        the key to use for push to talk. can be like <ctrl_r> or <alt_l> or <f1> or e or 1 or 2 or 3 etc
+```
+
+Defaults
+
+```yaml
+model_name: "base.en"
+mic_device: "default"
+inference_device: None
+activation_key: "<ctrl_r>"
+```
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pynput import keyboard
 
 
 class SpeachToText:
-    def __init__(self, device_name='default', model_name='base.en') -> None:
+    def __init__(self, model_name='base.en', mic_device='default', inference_device=None, activation_key=None) -> None:
         self.p = pyaudio.PyAudio()
         self.stream = None
 
@@ -19,15 +19,19 @@ class SpeachToText:
         device_index = None
         for i in range(self.p.get_device_count()):
             info = self.p.get_device_info_by_index(i)
-            if info['name'] == device_name:
+            if info['name'] == mic_device:
                 device_index = info['index']
                 break
 
         if device_index is None:
-            raise ValueError(f"Could not find audio device named {device_name}")
+            self.p.terminate()
+            raise ValueError(f'Could not find audio device named {mic_device}')
 
         # open the audio stream
         self.chunk_size = 512
+
+        # print that the mic is loading in orange which will later be overwritten with mic ready in green
+        print('\033[93m{}\033[00m'.format('loading mic...'), end='', flush=True)
 
         self.stream = self.p.open(format=pyaudio.paInt16,
                                   channels=1,
@@ -36,29 +40,42 @@ class SpeachToText:
                                   input=True,
                                   frames_per_buffer=self.chunk_size)
 
-        # setup whisper
-        self.model = whisper.load_model(model_name)
+        # overwrite the last print statement with mic ready in green
+        print('\r\x1b[K\033[92m{}\033[00m'.format('mic ready!'))
 
-        # make first inference on real data faster by forcing the model to finish loading
+        # print loading model in orange which will later be overwritten
+        print('\033[93m{}\033[00m'.format('loading model...'), end='', flush=True)
+
+        start_time = time.time()
+
+        # load the whisper model
+        self.model = whisper.load_model(name=model_name, device=inference_device)
+
+        # make first inference on real data faster by forcing the model to finish loading since in_memory does not seem to fully preload the model
         self.model.transcribe(np.zeros(201, dtype=np.float32))
+
+        end_time = time.time()
+
+        # overwrite the last print statement with model ready in green and the time in cyan
+        print('\r\x1b[K\033[92m{}\033[00m  \033[96m{:.2f}\033[00m seconds'.format('model ready!', end_time - start_time))
 
         # setup pynput
         self.keyboard = keyboard.Controller()
 
-        # print ready in green
-        print("\033[92m{}\033[00m".format("ready!"))
-
-        # F16 key
-        self.activation_key = keyboard.KeyCode.from_vk(269025095)
+        # Set the activation key
+        self.activation_key = activation_key
         self.activation_key_pressed = False
         self.listener_thread = None
+
+        # ignore input if the system is currently typing to avoid typing the activation key
+        self.currently_typing = False
 
         # list of the frames the audio stream has read
         self.frames = []
 
     # make destructor to close the stream and terminate the pyaudio instance
     def __del__(self):
-        # check ifthe thread is still running
+        # check if the thread is still running
         if self.listener_thread is not None and self.listener_thread.is_alive():
             print('thread still running')
             print(f'thread: {self.listener_thread}')
@@ -85,7 +102,7 @@ class SpeachToText:
         whisper_output = self.model.transcribe(data_to_transcribe)
 
         # return just the text
-        return whisper_output["text"]
+        return whisper_output['text']
 
     # listens to the audio stream and then processes the frames and types the output text
     def listen(self):
@@ -93,7 +110,7 @@ class SpeachToText:
 
         # print listening in cyan
         print()
-        print("\033[96m{}\033[00m".format("listening..."))
+        print('\033[96m{}\033[00m'.format('listening...'))
         self.stream.start_stream()
 
         while self.activation_key_pressed:
@@ -136,13 +153,19 @@ class SpeachToText:
         print(output_text)
 
         # type the output text
+        self.currently_typing = True
         self.keyboard.type(output_text)
+        self.currently_typing = False
 
         # reset the activation key
         self.activation_key_pressed = False
 
     # called when a key is pressed
     def on_press(self, key):
+        # ignore input if the system is currently typing to avoid typing the activation key
+        if self.currently_typing:
+            return
+
         # check if the designated key was pressed and that the key is not already being pressed
         if key == self.activation_key and not self.activation_key_pressed:
             # check if the thread is already running
@@ -172,15 +195,35 @@ class SpeachToText:
 if __name__ == '__main__':
     # get the args
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='medium.en', choices=['tiny.en', 'tiny', 'base.en', 'base', 'small.en',
-                        'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large'], help='the name of the model to use')
+    # add general description
+    parser.description = 'a small script that types what you say using whisper while holding a hotkey'
+    parser.add_argument('--model_name', type=str, default='base.en', choices=['tiny.en', 'tiny', 'base.en', 'base', 'small.en',
+                        'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large'], help='the name of the whisper model to use')
+    parser.add_argument('--mic_device', type=str, default='default', help='the name of the pyaudio input device to use')
+    parser.add_argument('--inference_device', type=str, default=None,
+                        help='the device to run the inference on. can be cpu, cuda, or cuda:<device number> will automatically select the best device if not specified')
+    parser.add_argument('--activation_key', type=str, default='<ctrl_r>', help='the key to use for push to talk. can be like <ctrl_r> or <alt_l> or <f1> or e or 1 or 2 or 3 etc')
     args = parser.parse_args()
 
-    # print model name
-    print(f'model: {args.model_name}')
+    # print the model
+    print(f'using model: \033[96m{args.model_name}\033[00m')
+
+    try:
+        hotkey_set = keyboard.HotKey.parse(args.activation_key)
+
+        if len(hotkey_set) > 1:
+            print('\033[91monly one key is supported right now\033[00m')
+            exit(1)
+
+        activation_key = hotkey_set[0]
+
+        print(f'using key: \033[96m{activation_key}\033[00m')
+    except ValueError:
+        print('\033[91minvalid activation key\033[00m')
+        parser.print_help()
 
     # run the program
     try:
-        SpeachToText(model_name=args.model_name).run()
+        SpeachToText(model_name=args.model_name, mic_device=args.mic_device, inference_device=args.inference_device, activation_key=activation_key).run()
     except KeyboardInterrupt:
-        print("Exiting...")
+        print('Exiting...')
